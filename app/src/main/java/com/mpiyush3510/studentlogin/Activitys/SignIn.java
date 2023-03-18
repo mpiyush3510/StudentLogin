d67e7f3fdd4a40efa23d3c21ba2f8ad12e02c8a0
Piyush Makwana <mpiyush3510@gmail.com>
2023-03-18 19:22:20 +0530

    SignUp && SignIn Xml Ok!

---
+++ app/src/main/java/com/mpiyush3510/studentlogin/Activitys/SignIn.java
@@ -0,0 +1,16 @@
+package com.mpiyush3510.studentlogin.Activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mpiyush3510.studentlogin.R;
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
